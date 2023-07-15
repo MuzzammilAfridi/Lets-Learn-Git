4b1c0fa587a1692b7692b46f539eed5c6c8b9d92
Muzzammil Afridi <muzzammilafridi1319@gmail.com>
2023-07-15 19:02:30 +0530

    Added implementation for dif.java

--- dif.java
+++ dif.java
@@ -1,3 +1,7 @@
 public class dif {
-    
+    public static void main(String[] args) {
+        int a = 5;
+        int b  = 6;
+        System.out.println("The Substraction of "+ a + "and" + b +"is" + a+(-b));
+    }  
 }
